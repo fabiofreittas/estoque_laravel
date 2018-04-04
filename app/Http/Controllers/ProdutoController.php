18194af4ff798a9b9dd8e5ac2c1744d0e6417df2
Fábio Freitas <fabio.freitas@unir.br>
2018-04-03 22:06:39 -0400

    Controller Pronto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Produto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class ProdutoController extends Controller
 {
@@ -14,7 +15,38 @@ class ProdutoController extends Controller
 
 
         }*/
+       // return view('produto.pesquisar')->with('produtos',$produtos);
+
+        $descricao=Input::get('descricao');
+        $produtos=Produto::where('descricao','like','%'.$descricao.'%')->get();
         return view('produto.pesquisar')->with('produtos',$produtos);
 
+
+    }
+
+    public function mostrar_inserir()
+    {
+        return view('produto.inserir');
+    }
+
+    public function inserir()
+    {
+        // Criando um novo objeto do tipo Produto
+        $produto = new Produto();
+
+        // Colocando os valores recebidos do formulário nos atributos do objeto $produto
+        $produto->descricao = Input::get('descricao');
+        $produto->quantidade = Input::get('quantidade');
+        $produto->valor = Input::get('valor');
+        $produto->data_vencimento = Input::get('data_vencimento');
+
+        // Salvando no banco de dados
+        $produto->save();
+
+        // Criado uma mensagem para o usuário
+        $mensagem = "Produto inserido com sucesso";
+
+        // Chamando a view produto.inserir e enviando a mensagem criada
+        return view('produto.inserir')->with('mensagem', $mensagem);
     }
 }
--- resources/views/produto/pesquisar.blade.php
+++ resources/views/produto/pesquisar.blade.php
@@ -11,6 +11,40 @@
 <body>
 <div class="container">
     <h1 class="mt-2">Pesquisa de produtos</h1>
+
+    @if(count($produtos) == 0)
+        <div class="alert alert-danger mt-2">Nenhum produto encontrado com essa descrição!</div>
+    @else
+
+        <form action="/produtos/pesquisar" method="post" class="form-inline mt-2">
+            <input type="hidden" name="_token" value="{{{ csrf_token() }}}">
+            <div class="form-group">
+                <label for="descricao">Descrição: </label>
+                <input type="text" id="descricao" name="descricao" class="form-control ml-2">
+            </div>
+            <input type="submit" class="btn btn-primary ml-2" value="Pesquisar">
+        </form>
+
+        <table class="table mt-2 text-center">
+            <tr>
+                <th>Id</th>
+                <th class="text-left">Descrição</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+                <th>Data de vencimento</th>
+            </tr>
+            @foreach ($produtos as $p)
+                <tr>
+                    <td>{{$p->id}}</td>
+                    <td class="text-left">{{$p->descricao}}</td>
+                    <td>{{$p->quantidade }}</td>
+                    <td>{{$p->valor }}</td>
+                    <td>{{$p->data_vencimento}}</td>
+                </tr>
+            @endforeach
+        </table>
+    @endif
+</div>
 </div>
 
 </body>
